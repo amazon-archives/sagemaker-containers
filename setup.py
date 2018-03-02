2a0c9a70d463997b7283aab71d784f98887b0c15
Winston Dong <win@amazon.com>
2018-03-01 18:05:36 -0800

    Move dependencies from extras_require to install_requires

--- setup.py
+++ setup.py
@@ -36,8 +36,8 @@ setup(
         'Programming Language :: Python :: 3.5',
     ],
 
-    install_requires=[],
+    install_requires=['Flask', 'boto3', 'six'],
     extras_require={
-        'test': ['tox', 'flake8', 'pytest', 'pytest-cov', 'pytest-xdist', 'mock', 'Flask', 'boto3']
+        'test': ['tox', 'flake8', 'pytest', 'pytest-cov', 'pytest-xdist', 'mock']
     },
 )
