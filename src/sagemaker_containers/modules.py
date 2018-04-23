e4ab451e00b6ac68a5c37629f21d06acff49912e
Marcio Dos Santos <mvs@amazon.com>
2018-04-23 16:30:04 -0700

    Add docstrings

--- src/sagemaker_containers/modules.py
+++ src/sagemaker_containers/modules.py
@@ -31,7 +31,13 @@ logger = logging.getLogger(__name__)
 DEFAULT_MODULE_NAME = 'default_user_module_name'
 
 
-def s3_download(url, dst):
+def s3_download(url, dst):  # type: (str, str) -> None
+    """Download a file from the S3.
+
+    Args:
+        url (str): the s3 url location of the file.
+        dst (str): the destination where the file will be saved.
+    """
     url = urlparse(url)
 
     if url.scheme != 's3':
@@ -43,7 +49,15 @@ def s3_download(url, dst):
     s3.Bucket(bucket).download_file(key, dst)
 
 
-def prepare(path, name):
+def prepare(path, name):  # type: (str, str) -> None
+    """Prepare a Python script (or module) to be imported as a module.
+
+    If the script does not contain a setup.py file, it creates a minimal setup.
+
+    Args:
+        path (str): path to directory with the script or module.
+        name (str): name of the script or module.
+    """
     if not os.path.exists(os.path.join(path, 'setup.py')):
         logging.info('Module %s does not provide a setup.py. Generating a minimal setup.' % name)
 
@@ -54,7 +68,12 @@ def prepare(path, name):
             f.write(os.linesep.join(lines))
 
 
-def install(path):
+def install(path):  # type: (str) -> None
+    """Install a Python module in the executing Python environment.
+
+    Args:
+        path (str):  Real path location of the Python module.
+    """
     if not sys.executable:
         raise RuntimeError('Failed to retrieve the real path for the Python executable binary')
     try:
@@ -63,7 +82,21 @@ def install(path):
         raise RuntimeError('Failed to pip install %s:%s%s' % (path, os.linesep, traceback.format_exc()))
 
 
-def download_and_import(url, name=DEFAULT_MODULE_NAME):
+def download_and_import(url, name=DEFAULT_MODULE_NAME):  # type: (str, str) -> module
+    """Download, Prepare and Install a compressed tar file from S3 as a module.
+
+    SageMaker Python SDK saves the user provided scripts as compressed tar files in S3
+    https://github.com/aws/sagemaker-python-sdk#mxnet-sagemaker-estimators.
+
+    This function downloads this compressed file, transforms it as a module, and installs it.
+
+    Args:
+        url (str): the s3 url location of the file.
+        name (str): name of the script or module.
+
+    Returns:
+
+    """
     with tempfile.NamedTemporaryFile() as tmp:
         s3_download(url, tmp.name)
 
