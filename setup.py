7325d04ca0873feb27559b2bb26bb40cb254c829
Marcio Dos Santos <mvs@amazon.com>
2019-01-10 12:21:26 -0800

    Update version number

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ gethostname = setuptools.Extension('gethostname',
 
 setuptools.setup(
     name='sagemaker_containers',
-    version='2.4.1',
+    version='2.4.2',
     description='Open source library for creating containers to run on Amazon SageMaker.',
 
     packages=packages,
