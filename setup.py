71fb23d76a8d0206974e39dcfdff3b0f72300b56
Nadia Yakimakha <32335935+nadiaya@users.noreply.github.com>
2019-01-22 23:17:08 -0800

    Use correct numpy version.

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ packages.append('sagemaker_containers.etc')
 required_packages = [
     # freeze numpy version because of the python2 bug
     # in 16.0: https://github.com/numpy/numpy/pull/12754
-    'numpy<=15.4', 'boto3', 'six', 'pip', 'flask', 'gunicorn', 'typing',
+    'numpy<=1.15.4', 'boto3', 'six', 'pip', 'flask', 'gunicorn', 'typing',
     'gevent', 'inotify_simple', 'werkzeug', 'paramiko==2.4.2', 'psutil==5.4.8'
 ]
 
