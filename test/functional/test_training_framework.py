f3ab181fff163e15df1aab06878df98163ee912f
Ajay Karpur <akarpur@amazon.com>
2019-09-17 15:45:48 -0700

    Add print statements for debugging

--- test/functional/test_training_framework.py
+++ test/functional/test_training_framework.py
@@ -530,6 +530,11 @@ def test_script_mode_client_error(training_fn, capture_error):
         training_fn(capture_error)
 
     message = str(e.value)
+
+    print("Before message...")
+    print(message)
+    print("After message.")
+
     assert "ExecuteUserScriptError" in message
     if capture_error:
         assert "ZeroDivisionError" in message
@@ -558,6 +563,11 @@ def test_script_mode_client_import_error(training_fn, capture_error):
         training_fn(capture_error)
 
     message = str(e.value)
+
+    print("Before message...")
+    print(message)
+    print("After message.")
+
     assert "InstallModuleError:" in message
 
     # fmt: off
